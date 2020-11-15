e4edbe9db5f1992ade5fbf1f7eac694e40c7fc3a
Artur <artmucha@bitbucket.org>
2020-11-15 10:40:10 +0100

    saved images on server

--- client/pages/api/games/index.js
+++ client/pages/api/games/index.js
@@ -1,7 +1,24 @@
+import fs from 'fs';
+import https from 'https';
+
 import dbConnect from 'utils/dbConnect';
 import Game from 'models/Game';
 
 dbConnect();
+
+const download = (url, dest) => {
+  var file = fs.createWriteStream(dest);
+  https.get(url, (response) => {
+    response.pipe(file);
+    file.on('finish', () => {
+      file.close(() => {
+        console.log('success');
+      });
+    });
+  });
+
+  return file.path;
+}
 
 export default async (req, res) => {
   const { method } = req;
@@ -17,6 +34,20 @@ export default async (req, res) => {
       break;
     case 'POST':
       try {
+        const cover = download(req.body.cover, `./public/media/games/covers/${req.body.slug}.jpg`);
+        req.body.cover = cover.slice(8);
+
+        const images = req.body.images.map(({id, image}) => {
+          image = download(image, `./public/media/games/images/${req.body.slug}-${id}.jpg`);
+          image = image.slice(8);
+          return {
+            id: id,
+            image: image
+          };
+        });
+
+        req.body.images = images;
+
         const game = await Game.create(req.body);
         res.status(201).json({success: true, data: game});
       } catch (error) {
